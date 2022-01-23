9ac0f0b5f20feb0dd3889a07c8b7ef07011533b4
Michelle Curran <michellecurran@Michelles-MacBook-Pro.local>
2022-01-22 22:30:20 -0500

    Added 3.5.2 changes to print out the unique candidate names

--- PyPoll.py
+++ PyPoll.py
@@ -7,6 +7,12 @@ file_to_load = os.path.join("Desktop","DataAnalyticsBootcamp","03-Python","elect
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("Desktop","DataAnalyticsBootcamp","03-Python","election-analysis","analysis", "election_analysis.txt")
 
+# 1. Initialize a total vote counter.
+total_votes = 0
+
+# Candidate Options
+candidate_options = []
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -14,6 +20,27 @@ with open(file_to_load) as election_data:
     # Read and print the header row.
     headers = next(file_reader)
     print(headers)
-
-
     
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # 2. Add to the total vote count.
+        total_votes += 1
+
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+        # Add the candidate name to the candidate list.
+        #candidate_options.append(candidate_name)
+# 3. Print the total votes.
+# print(total_votes)
+
+# Print the candidate list.
+print(candidate_options)
+
+
+

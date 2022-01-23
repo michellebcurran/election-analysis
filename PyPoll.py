f7f52b0ce81efba449746ffdd8cc27677d876e2b
Michelle Curran <michellecurran@Michelles-MacBook-Pro.local>
2022-01-23 12:11:57 -0500

    3.5.5 update

--- PyPoll.py
+++ PyPoll.py
@@ -1,4 +1,4 @@
-# 3.5.4  Module 3
+# 3.5.5  Module 3
 # Add our dependencies.
 import csv
 import os
@@ -15,6 +15,11 @@ candidate_options = []
 
 # 1. Declare the empty dictionary.
 candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
@@ -57,7 +62,30 @@ for candidate_name in candidate_votes:
     # 3. Calculate the percentage of votes.
     vote_percentage = float(votes) / float(total_votes) * 100
     # 4. Print the candidate name and percentage of votes.
-    print(f"{candidate_name}: received {vote_percentage:.2f}% of the vote.")
+    # print(f"{candidate_name}: received {vote_percentage:.2f}% of the vote.")
+
+    #  To do: print out each candidate's name, vote count, and percentage of
+    # votes to the terminal.
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+    # Determine winning vote count and candidate
+    # Determine if the votes is greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+         # If true then set winning_count = votes and winning_percent =
+         # vote_percentage.
+         winning_count = votes
+         winning_percentage = vote_percentage
+         # And, set the winning_candidate equal to the candidate's name.
+         winning_candidate = candidate_name
+
+#  To do: print out the winning candidate, vote count and percentage to
+#   terminal.
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
 
 # Print the candidate list.
 #print(candidate_options)
